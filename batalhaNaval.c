ed6b9493a9454ee20987f9876aa9884292bb1f97
Felcker <mfelcker@gmail.com>
2025-05-17 02:27:17 +0000

    DESAFIO NIVEL MESTRE

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -3,6 +3,19 @@
 #define LINHAS 10 // Definindo o tamanho da linha
 #define COLUNAS 10 // Definindo o tamanho da coluna
 
+ // Impressão do tabuleiro
+ void exibirTabuleiro(int tabuleiro[LINHAS][COLUNAS]) {
+    printf("Tabuleiro de BATALHA NAVAL!:\n");
+    for (int i = 0; i < LINHAS; i++) 
+    {
+        for (int j = 0; j < COLUNAS; j++) 
+        {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 int main () {
 
     // Criação da matriz 10x10
@@ -22,26 +35,76 @@ int main () {
 
     printf("\n\n");
 
-    // Posicionamento do navio no tabuleiro
+    // Posicionamento dos navios no tabuleiro
     for (int i = 0; i < 3; i++)
     {
-        tabuleiro[3][3 + i] = 3; // Define a posição do navio horizontal
+        tabuleiro[4][3 + i] = 3; // Define a posição do navio horizontal
         tabuleiro[5 + i][7] = 3; // Define a posição do navio vertical]
         tabuleiro[0 + i][0 + i] = 3; // Define a posição do navio diagonal
         tabuleiro[0 + i][9 - i] = 3; // Define a posição do navio diagonal
     }
-    
-    // Impressão do novo tabuleiro
-    printf("Tabuleiro com navio:\n");
-    for (int i = 0; i < LINHAS; i++) 
-    {
-        for (int j = 0; j < COLUNAS; j++) 
-        {
-            printf("%d ", tabuleiro[i][j]); // Imprime o valor da posição
-        }
-        printf("\n");
-    }
 
+    // Criação dos poderes especiais
+
+    // Criação do poder do cone
+    int cone[3][5];
+        for (int i = 0; i < 3; i++) 
+        {
+            for (int j = 0; j < 5; j++) 
+            {
+                if (j >= 2 - i && j <= 2 + i) 
+                {
+                    cone[i][j] = 5; // Define a posição do cone
+                } 
+                else 
+                {
+                    cone[i][j] = 0; // Define a posição vazia
+                }
+            }
+        printf("\n");
+        }
+
+    // Criação do poder da cruz
+    int cruz[3][5];
+        for (int i = 0; i < 3; i++) 
+        {
+            for (int j = 0; j < 5; j++) 
+            {
+                if (i == 1 || j == 2) 
+                {
+                    cruz[i][j] = 5; // Define a posição do cruz
+                } 
+                else 
+                {
+                    cruz[i][j] = 0; // Define a posição vazia
+                }    
+            }
+        printf("\n");
+        }
+
+    // Criação do poder do octaedro
+    int octaedro[3][5];
+        for (int i = 0; i < 3; i++) 
+        {
+            for (int j = 0; j < 5; j++) 
+            {
+                if ((i == 0 && j == 2) || (i == 1 && (j == 1 || j == 2 || j == 3) || (i == 2 && j == 2)))
+                {
+                    octaedro[i][j] = 5; // Define a posição do octaedro
+                } 
+                else 
+                {
+                    octaedro[i][j] = 0; // Define a posição vazia
+                }    
+            }
+        printf("\n");
+        }
+  
+    
+
+    // Exibição do tabuleiro com os navios
+    exibirTabuleiro(tabuleiro); // Chama a função para exibir o tabuleiro
+   
     printf("\n");
 
     return 0;
